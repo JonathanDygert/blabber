123a2907424a2ff0456ccc36c686735e1bf720f8
Jonathan Dygert <jonathandygert@gmail.com>
2020-03-04 12:50:34 -0500

    Implement blabber api

--- src/app.py
+++ src/app.py
@@ -1,11 +1,41 @@
-"""A basic flask app."""
+"""A flask app for the blabber api."""
 
-from flask import Flask, jsonify
+from time import time
+from uuid import uuid4
+
+from flask import Flask, abort, jsonify, request
 
 APP = Flask(__name__)
 
+BLABS = {}
 
-@APP.route("/greetings")
-def greetings():
-    """Return a list of greetings."""
-    return jsonify(["Hello, world!", "Hi there!", "Greetings!"])
+
+@APP.route("/api/blabs", methods=["GET"])
+def get_blabs():
+    """Get all blabs created since the specified timestamp."""
+    since = int(request.args.get("createdSince", 0))
+
+    return jsonify([blab for blab in BLABS.values() if blab["postTime"] >= since])
+
+
+@APP.route("/api/blabs", methods=["POST"])
+def post_blabs():
+    """Add a new blab."""
+    blab = request.json
+
+    blab_id = uuid4()
+    BLABS[blab_id] = blab
+
+    blab["id"] = blab_id
+    blab["postTime"] = int(time())
+
+    return jsonify(status=201, result=blab)
+
+
+@APP.route("/api/blabs/<id>", methods=["DELETE"])
+def delete_blabs(blab_id):
+    """Remove a blab with the given id."""
+    try:
+        del BLABS[blab_id]
+    except KeyError:
+        abort(404)
